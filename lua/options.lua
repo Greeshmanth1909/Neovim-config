cb3dd30efcb261e264b01bcf91ff01ef4506248f
Greeshmanth1909 <91905738+Greeshmanth1909@users.noreply.github.com>
2024-04-05 23:10:58 +0530

    lsp works, kinda

---
+++ lua/options.lua
@@ -0,0 +1,35 @@
+-- line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- window management
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- line wrapping
+vim.opt.wrap = false
+
+-- tabs and spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- scroll offset
+vim.opt.scrolloff = 999
+
+-- virtual editing in Visual block mode
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+-- ignore case
+vim.opt.ignorecase = true
+
+-- color
+vim.opt.termguicolors = true
+
+-- Setup leader key
+vim.g.mapleader = " "
---
+++ lua/plugins.lua
@@ -0,0 +1,107 @@
+-- Setup Lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    { 
+        "rebelot/kanagawa.nvim",
+        config = function()
+            vim.cmd.colorscheme("kanagawa-wave")
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "javascript", "python", "go"},
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<CR>", -- set to `false` to disable one of the mappings
+                        node_incremental = "<CR>",
+                        scope_incremental = "<TAB>",
+                        node_decremental = "<S-TAB>",
+                    },
+                },
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
+
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                            -- nvim_buf_set_keymap) which plugins like which-key display
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            -- You can also use captures from other query groups like `locals.scm`
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V', -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    {
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "nvim-lua/plenary.nvim",
+    },
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.5",
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[s]earch [h]elp' })
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({})
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {"mason.nvim"},
+        config = function()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers ({
+                function (server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {}
+                end,
+            })
+        end
+    },
+})
